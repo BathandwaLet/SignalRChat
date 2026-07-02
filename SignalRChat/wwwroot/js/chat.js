3605145c212945983b18b8d61afbf206323ffd1b
Bathandwa <bmap750@gmail.com>
2026-07-02 10:21:33 +0200

    feat(chat.js): Added chat.js to:     1-Create and start connection.     2-Add a submit button to send messages to the hub.     3-Add a connection object handler.

---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,29 @@
+"use-strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disables the send button until connected
+document.getElementById("sendButton").disabled = true;
+
+connection.on("RecieveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    
+    //Assigning user supplied string 
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err){
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+})
