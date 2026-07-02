abefb33573031fdfb97ada82a2395b707616ed29
Bathandwa <bmap750@gmail.com>
2026-07-02 09:49:40 +0200

    feat: Added async SendMessage method to handle client-server communicatioin.

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -2,7 +2,10 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs;
 
-public class ChatHub
+public class ChatHub : Hub
 {
-    
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("RecieveMessage", user, message);
+    }
 }
